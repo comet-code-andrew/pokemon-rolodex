b3bdbcb7682031d3c7e2164debc8144a000ad78b
Andrew Heyer <andrewbheyer@gmail.com>
2023-03-09 15:02:18 -0800

    loading screen added to cards as well

--- src/components/pokemon-list/pokemon-list.component.jsx
+++ src/components/pokemon-list/pokemon-list.component.jsx
@@ -1,13 +1,34 @@
 import PokemonListCard from "../pokemon-list-card/pokemon-list-card.component";
+import Loader from "../loader/loader.component";
+import PokemonSetListCard from "../pokemon-set-list-card/pokemon-set-list-card.component";
 
-const PokemonList = ( {props} ) => {
-  return(
+const PokemonList = ( {isLoading, props} ) => {
+  return isLoading ?
+    (
+     <Loader/>
+    ) : (
     <div className='card-list'>
       { props.map( ( pokemon ) => {
         return (<PokemonListCard props={pokemon}/>)
       })}
     </div>
   )
+
+
+  // return isLoading ?
+  //   (
+  //     <Loader/>
+  //   ) : (
+  //     <div className='card-list'>
+  //       { props.map( ( set ) => {
+  //         return (<PokemonSetListCard props={set}/>)
+  //       })}
+  //     </div>
+  //   )
+
+
+
+
 }
 
 export default PokemonList;
--- src/routes/pokemon/pokemon.component.jsx
+++ src/routes/pokemon/pokemon.component.jsx
@@ -1,23 +1,34 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import PokemonList from "../../components/pokemon-list/pokemon-list.component";
+import axios from "axios";
 
 const Pokemon = () => {
   const params = useParams();
 
   const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState([]);
 
-  useEffect(() => {
-    fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${params.set}`)
-      .then( ( response) => response.json()  )
-      .then( ( data ) => setPokemon( data.data ) )
-  }, [])
+  // useEffect(() => {
+  //   fetch(`https://api.pokemontcg.io/v2/cards?q=set.id:${params.set}`)
+  //     .then( ( response) => response.json()  )
+  //     .then( ( data ) => setPokemon( data.data ) )
+  // }, [])
 
-  console.log(pokemon)
+  useEffect (() => {
+    const getItems = async () => {
+      const result = await axios(
+        `https://api.pokemontcg.io/v2/cards?q=set.id:${params.set}` //Endpoint and parameter or base Url
+      )
+      setPokemon(result.data.data)//sets the data to appear
+      setLoading(false) //stop loading when data is fetched
+    }
+    getItems()
+  },[])
 
   return (
     <>
-      <PokemonList props={pokemon}/>
+      <PokemonList isLoading={loading} props={pokemon}/>
     </>
   )
 }
