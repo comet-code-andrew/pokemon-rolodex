4a379c3a85afda1caabcf3f44d427dce04d43ab1
Andrew Heyer <andrewbheyer@gmail.com>
2023-03-09 15:18:32 -0800

    adding a title to the site page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import {Routes, Route } from 'react-router-dom'
 import Home from "./routes/home/home.component";
 import Pokemon from "./routes/pokemon/pokemon.component";
+import './App.css'
 
 function App() {
   return (
+    <>
+    <h1 className="app-title">Savage Pokédex</h1>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='set/:set' element={<Pokemon/>}/>
     </Routes>
+    </>
   );
 };
 
